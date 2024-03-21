afe3c4b5df3d9b7d6542adf309e6eadc75515245
Alex Lovergne <95444080+ArcenIce@users.noreply.github.com>
2024-03-21 14:45:14 +0100

    Ajout affichage

--- src/front/src/components/PalsDetails.vue
+++ src/front/src/components/PalsDetails.vue
@@ -1,31 +1,93 @@
 <template>
-  <p v-for="(pal, index) in pals" :key="index">
-    {{ pal }}
-  </p>
+
+  <div v-if="props.pals" class="py-2 text-subtitle-1">
+    <v-row no-gutters>
+      <v-col cols="9">
+          <span class="text-primary">Nom: </span>{{ props.pals.name }}<br>
+          <span class="text-primary">Taille: </span>{{ props.pals.size }}<br>
+          <span class="text-primary">Rareté: </span>{{ props.pals.rarity }}<br>
+          <span class="text-primary">Prix: </span>{{ props.pals.price }}G<br>
+      </v-col>
+      <v-col>
+        <img :src="backBaseUrl + props.pals.image" width="100%"/>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col>
+        <span class="text-primary">Description: </span>{{ props.pals.description }}G<br>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col>
+        <span class="text-primary">Drops: </span>
+        <ul class="pl-6">
+          <li v-for="drop in props.pals.drops" :key="drop">
+            {{ drop }}
+          </li>
+        </ul>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col>
+        <span class="text-primary">Stats: </span><br>
+        <div class="px-4">
+          <span class="text-primary">Défence: </span>{{ props.pals.stats.defense }}<br>
+          <span class="text-primary">Vie: </span>{{ props.pals.stats.hp }}<br>
+          <span class="text-primary">Stamina: </span>{{ props.pals.stats.stamina }}<br>
+          <span class="text-primary">Support: </span>{{ props.pals.stats.support }}<br>
+          <span class="text-primary">Nourriture: </span>{{ props.pals.stats.food }}<br>
+          <span class="text-primary">Attaque: </span><br>
+          <div class="px-4">
+            <span class="text-primary">Melee: </span>{{ props.pals.stats.attack.melee }}<br>
+            <span class="text-primary">Distance: </span>{{ props.pals.stats.attack.ranged }}<br>
+          </div>
+          <span class="text-primary">Vitesse: </span><br>
+          <div class="px-4">
+            <span class="text-primary">Monté: </span>{{ props.pals.stats.speed.ride }}<br>
+            <span class="text-primary">Courir: </span>{{ props.pals.stats.speed.run }}<br>
+            <span class="text-primary">Marche: </span>{{ props.pals.stats.speed.walk }}<br>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col>
+        <span class="text-primary">Skills: </span>
+
+        <v-card class="mx-n7 my-2">
+          <v-tabs density="compact" fixed="true" slim v-model="tab" bg-color="primary">
+            <v-tab v-for="skill in props.pals.skills" :key="skill" :value="skill.level" class="text-body-2">{{ skill.name }}</v-tab>
+          </v-tabs>
+
+          <v-card-text>
+            <v-window v-model="tab">
+              <v-window-item v-for="skill in props.pals.skills" :key="skill" :value="skill.level">
+                <span class="text-primary">Niveau: </span>{{ skill.level }}<br>
+                <span class="text-primary">Nom: </span>{{ skill.name }}<br>
+                <span class="text-primary">Type: </span>{{ skill.type }}<br>
+                <span class="text-primary">Puissance: </span>{{ skill.power }}<br>
+                <span class="text-primary">Cooldown: </span>{{ skill.cooldown }}<br>
+                <span class="text-primary">Description: </span>{{ skill.description }}<br>
+              </v-window-item>
+            </v-window>
+          </v-card-text>
+        </v-card>
+
+      </v-col>
+    </v-row>
+  </div>
   
-  <!--
-  <v-sheet
-    class="d-flex align-content-space-around flex-wrap box-middle no-background"
-    min-height="200"
-  >
-    <v-sheet
-      v-for="(pal, index) in pals" :key="index" class="pal-items"
-    >
-      <p>{{ pal.name }}</p>
-    </v-sheet>
-  </v-sheet>-->
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-import { defineProps } from 'vue';
+import { defineProps, ref } from 'vue';
+
+const backBaseUrl = import.meta.env.VITE_BACK_BASE_URL;
+
+const tab = ref(null);
 
 const props = defineProps({
   pals: Object
-});
-
-onMounted(() => {
-  console.log('pals:', props.pals);
 });
 </script>
 
