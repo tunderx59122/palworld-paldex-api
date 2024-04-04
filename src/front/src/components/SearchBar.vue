189c9e973683246b73c36c4c42f03f1c60481fc2
thomas <thomasipod59122@gmail.com>
2024-04-04 15:09:55 +0200

    EVOLUTION : adding a SearchBar

---
+++ src/front/src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<!-- SearchBar.vue -->
+<template>
+    <div>
+      <input type="text" v-model="searchQuery" @change="$emit('filteredPals', searchQuery)" placeholder="Search...">
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    data() {
+      return {
+        searchQuery: '',
+        // Simulated search results
+        searchResults: []
+      }
+    },
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your CSS styles here */
+  </style>
+  
